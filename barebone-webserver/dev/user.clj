98f869963275794dfc3fb096ba13f293e919b7f5
chpill <chpill@example.com>
2023-10-27 19:29:05 +0200

    Improve dev experience when interacting with the barebone user.clj
    The file will now be "refreshed" like any other of project own files. It
    uses defonce to avoid losing the reference to the live system in case we
    evaluate the whole buffer.

--- barebone-webserver/dev/user.clj
+++ barebone-webserver/dev/user.clj
@@ -1,11 +1,8 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :refer [set-refresh-dirs
-                                                  refresh-all]]
-            [demo-closeable.barebone-webserver :refer [run-with-webserver]]))
+  (:require [demo-closeable.barebone-webserver :refer [run-with-webserver]]))
 
-(set-refresh-dirs "src" "test")
 
-(def *live-server (atom (future ::not-initialized-yet)))
+(defonce *live-server (atom (future ::not-initialized-yet)))
 
 (defn start! []
   (reset! *live-server (future (run-with-webserver {:port 54321}
@@ -14,5 +11,4 @@
   (future-cancel @*live-server))
 
 (comment (start!)
-         (stop!)
-         (refresh-all))
+         (stop!))
